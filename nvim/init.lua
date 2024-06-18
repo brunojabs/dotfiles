af538776ed6e0ac7cf8e563a9e83b396050d9cc5
Bruno Gomes <brunojabs@gmail.com>
2024-06-18 12:01:55 -0300

    Adjust and add setup to missing function

--- nvim/init.lua
+++ nvim/init.lua
@@ -57,8 +57,13 @@ vim.g['test#neovim#term_position'] = "vert"
 vim.g['test#strategy'] = "neovim"
 
 require("lazy").setup({
-  'nvim-lualine/lualine.nvim',
-  'kyazdani42/nvim-web-devicons',
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    opts = {}
+  },
   'j-hui/fidget.nvim',
   { 'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons', lazy = false },
   {
@@ -100,7 +105,53 @@ require("lazy").setup({
     branch = 'v2', -- optional but strongly recommended
   },
   {
+    'stevearc/dressing.nvim',
+    opts = {}
+  },
+  {
     'lewis6991/gitsigns.nvim',
+    config = function()
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        -- Actions
+        map('n', '<leader>hs', gs.stage_hunk)
+        map('n', '<leader>hr', gs.reset_hunk)
+        map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+        map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+        map('n', '<leader>hS', gs.stage_buffer)
+        map('n', '<leader>hu', gs.undo_stage_hunk)
+        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hD', function() gs.diffthis('~') end)
+        map('n', '<leader>td', gs.toggle_deleted)
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      end
+    end
   },
   {
     'maxmx03/solarized.nvim',
@@ -160,19 +211,19 @@ require("lazy").setup({
     config = function()
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all"
-        ensure_installed = { "javascript", "lua", "rust", "vim", },
+        ensure_installed = { "javascript", "lua", "rust", "vim", "typescript" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
 
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = false,
+        auto_install = true,
 
         highlight = {
           -- `false` will disable the whole extension
           enable = true,
-          additional_vim_regex_highlighting = false,
+          additional_vim_regex_highlighting = { "markdown" },
           disable = { "gitcommit" }
         },
         incremental_selection = {
@@ -186,14 +237,10 @@ require("lazy").setup({
         },
       }
     end
-
   },
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    'neovim/nvim-lspconfig',
     dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
       { 'williamboman/mason.nvim' },
       { 'williamboman/mason-lspconfig.nvim' },
 
@@ -209,149 +256,107 @@ require("lazy").setup({
       -- Snippets
       { 'L3MON4D3/LuaSnip' },
       { 'rafamadriz/friendly-snippets' },
-    }
-  },
+    },
 
-  config = function()
-    local lsp = require('lsp-zero')
-
-    lsp.preset('recommended')
-
-    lsp.ensure_installed({
-      'tsserver',
-      'rust_analyzer',
-    })
-
-    lsp.configure('svelte', {
-      settings = {
-        svelte = {
-          ['enable-ts-plugin'] = true
-        }
-      }
-    })
-
-    -- Setup nvim-cmp.
-    local cmp = require 'cmp'
-    local has_words_before = function()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
-    lsp.on_attach(function(client, bufnr)
-      lsp.default_keymaps({ buffer = bufnr })
-
-      local opts = { buffer = bufnr, remap = false }
-
-      if client.name == "eslint" then
-        vim.cmd.LspStop('eslint')
-        return
+    config = function()
+      -- Setup nvim-cmp.
+      local cmp = require('cmp')
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local builtin = require('telescope.builtin')
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-      vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-      vim.keymap.set("n", "[e", vim.diagnostic.goto_next, opts)
-      vim.keymap.set("n", "]e", vim.diagnostic.goto_prev, opts)
-      vim.keymap.set("n", "<leader><space>", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("v", "<leader><space>", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "gh", builtin.lsp_references, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.rename, opts)
-      vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-    end)
-
-    require("mason-lspconfig").setup {
-      automatic_installation = false,
-    }
-
-    require 'lspconfig'.glint.setup {}
-
-    lsp.set_sign_icons({
-      error = '✘',
-      warn = '▲',
-      hint = '⚑',
-      info = '»'
-    })
-
-    vim.diagnostic.config({
-      float = { source = "always" },
-      virtual_text = false,
-      signs = true,
-    })
-
-    lsp.nvim_workspace()
-    lsp.setup()
-
-    local luasnip = require("luasnip");
-
-    cmp.setup({
-      preselect = cmp.PreselectMode.None,
-      completion = {
-        completeopt = 'menu,menuone,noinsert'
-      },
-      mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      require("fidget").setup({})
+      require("mason").setup()
+      require("mason-lspconfig").setup {
+        ensure_installed = {
+          "rust_analyzer",
+          "tsserver"
+        },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup {
+              capabilities = capabilities
+            }
           end
-        end, { "i", "s" }),
-
-        ["<Down>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          end
-        end, { "i", "s" }),
-
-        ["<Up>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          end
-        end, { "i", "s" }),
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        }
       }
-    })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline', keyword_length = 2 }
+      vim.diagnostic.config({
+        float = { source = "always" },
+        virtual_text = false,
+        signs = true,
       })
-    })
 
-    require('luasnip.loaders.from_vscode').lazy_load()
+      local luasnip = require("luasnip");
+      cmp.setup({
+        preselect = cmp.PreselectMode.None,
+        completion = {
+          completeopt = 'menu,menuone,noinsert'
+        },
+        mapping = {
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+          end, { "i", "s" }),
 
-    vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
-  end
+          ["<Down>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            end
+          end, { "i", "s" }),
+
+          ["<Up>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            end
+          end, { "i", "s" }),
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline', keyword_length = 2 }
+        })
+      })
+
+      require('luasnip.loaders.from_vscode').lazy_load()
+
+      vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+    end
+  },
 })
 
 -- REMAP
@@ -371,3 +376,23 @@ vim.keymap.set("n", "<leader>c", ":Cargo c<CR>")
 vim.keymap.set("n", "<leader>C", ":Cargo clippy<CR>")
 vim.keymap.set("n", "<leader>t", ":TestFile<CR>")
 vim.keymap.set("n", "<leader>T", ":TestFile -strategy=neovim_sticky<CR>")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[e", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "]e", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader><space>", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("v", "<leader><space>", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gh", builtin.lsp_references, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.rename, opts)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+  end
+})
