e71f75577f6c81e52d29bf0e5aa552026edf922b
Bruno Gomes <brunojabs@gmail.com>
2024-07-26 11:48:06 -0300

    Use automatic sessions for Neovim

--- nvim/lua/brunojabs/init.lua
+++ nvim/lua/brunojabs/init.lua
@@ -1,3 +1,12 @@
 require("brunojabs.set")
 require("brunojabs.remap")
 require("brunojabs.lazy")
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  pattern = "*",
+  callback = function()
+    if vim.g.savesession then
+      vim.api.nvim_command("mks!")
+    end
+  end
+})
